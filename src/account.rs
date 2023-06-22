1c9083ca492ac858784ad7ce7f3c49620bb8b574
root <rahul.soshte4771@gmail.com>
2023-06-22 14:37:44 +0000

    using rustfmt, clippy

--- src/account.rs
+++ src/account.rs
@@ -1,8 +1,8 @@
-use std::ops::AddAssign;
-use std::{str::FromStr, ops::Add};
 use num_bigint::BigUint;
+use std::ops::AddAssign;
+use std::str::FromStr;
 
-use stellar_strkey::ed25519::{PublicKey, MuxedAccount};
+use stellar_strkey::ed25519::{MuxedAccount, PublicKey};
 
 pub struct Account {
     account_id: String,
@@ -11,19 +11,17 @@ pub struct Account {
 
 impl Account {
     pub fn new(account_id: &str, sequence: &str) -> Result<Self, Box<dyn std::error::Error>> {
+        let muxed_key = MuxedAccount::from_string(account_id);
 
-        
-    let muxed_key = MuxedAccount::from_string(account_id);
+        if muxed_key.is_ok() {
+            return Err("accountId is an M-address; use MuxedAccount instead".into());
+        }
 
-    if muxed_key.is_ok() {
-        return Err("accountId is an M-address; use MuxedAccount instead".into());
-    }
+        let key = PublicKey::from_string(account_id);
 
-       let key =  PublicKey::from_string(account_id);
-
-       if key.is_err() {
+        if key.is_err() {
             return Err("accountId is invalid".into());
-       }
+        }
 
         let sequence = BigUint::from_str(sequence)?;
         Ok(Self {
@@ -41,6 +39,6 @@ impl Account {
     }
 
     pub fn increment_sequence_number(&mut self) {
-        self.sequence.add_assign(BigUint::from(1 as u32));
+        self.sequence.add_assign(BigUint::from(1_u32));
     }
 }
