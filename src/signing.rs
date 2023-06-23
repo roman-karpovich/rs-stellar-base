0e6720b3a88aa4057332d2c7845abfb84f94f342
root <rahul.soshte4771@gmail.com>
2023-06-23 16:14:43 +0000

    wasm32 conditional compilation with nacl

--- src/signing.rs
+++ src/signing.rs
@@ -30,28 +30,37 @@ fn check_fast_signing() -> ActualMethods {
 
 #[cfg(not(target_arch = "wasm32"))]
 fn check_fast_signing() -> ActualMethods {
+    
     check_fast_signing_native()
+
 }
 
 #[cfg(all(target_arch = "wasm32", target_os = "unknown"))]
 fn check_fast_signing_browser() -> ActualMethods {
+
     fn generate(secret_key: &[u8]) -> [u8; 32] {
         let secret_key_u8: &[u8; 32] = secret_key.try_into().unwrap();
-        let nacl_keys = tweetnacl::sign::keypair_from_seed(secret_key_u8);
-        nacl_keys.public_key
+        let nacl_keys = nacl::sign::generate_keypair(secret_key_u8);
+        nacl_keys.pkey
     }
 
     fn sign(data: &[u8], secret_key: &[u8]) -> [u8; 64] {
-        let data_u8: &[u8; N] = data.try_into().unwrap();
-        let secret_key_u8: &[u8; N] = secret_key.try_into().unwrap();
-        tweetnacl::sign::detached(data_u8, secret_key_u8)
+        let mut data_u8 = data;
+        let mut secret_key_u8 = secret_key;
+        let signed_msg = nacl::sign::sign(&mut data_u8, &mut secret_key_u8).unwrap();
+        let mut signature = [0u8; 64];
+        
+        for i in 0..signed_msg.len() {
+            signature[i] = signed_msg[i];
+        }
+        signature
     }
 
     fn verify(data: &[u8], signature: &[u8], public_key: &[u8]) -> bool {
-        let data_u8: &[u8; N] = data.try_into().unwrap();
-        let signature_u8: &[u8; N] = signature.try_into().unwrap();
-        let public_key_u8: &[u8; N] = public_key.try_into().unwrap();
-        tweetnacl::sign::detached_verify(data_u8, signature_u8, public_key_u8)
+        let data_u8 = data;
+        let signature_u8 = signature;
+        let public_key_u8 = public_key;
+        nacl::sign::verify(data_u8, signature_u8, public_key_u8).unwrap()
     }
 
     ActualMethods {
