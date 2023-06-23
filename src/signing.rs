18e099625bb80f9ce272c14c11913a2cf69eab9e
root <rahul.soshte4771@gmail.com>
2023-06-23 12:01:24 +0000

    excluding libsodium-sys when wasm32

--- src/signing.rs
+++ src/signing.rs
@@ -2,7 +2,7 @@
 use lazy_static::lazy_static;
 
 lazy_static! {
-    static ref ACTUAL_METHODS: ActualMethods = check_fast_signing_rs();
+    static ref ACTUAL_METHODS: ActualMethods = check_fast_signing();
 }
 
 struct ActualMethods {
@@ -23,7 +23,43 @@ fn generate(secret_key: &[u8]) -> [u8; 32] {
     (ACTUAL_METHODS.generate)(secret_key)
 }
 
+#[cfg(all(target_arch = "wasm32", target_os = "unknown"))]
+fn check_fast_signing() -> ActualMethods {
+    check_fast_signing_browser()
+}
 
+#[cfg(not(target_arch = "wasm32"))]
+fn check_fast_signing() -> ActualMethods {
+    check_fast_signing_rs()
+}
+
+#[cfg(all(target_arch = "wasm32", target_os = "unknown"))]
+fn check_fast_signing_browser() -> ActualMethods {
+    fn generate(secret_key: &[u8]) -> [u8; 32] {
+        let secret_key_u8: &[u8; 32] = secret_key.try_into().unwrap();
+        let nacl_keys = tweetnacl::sign::keypair_from_seed(secret_key_u8);
+        nacl_keys.public_key
+    }
+
+    fn sign(data: &[u8], secret_key: &[u8]) -> [u8; 64] {
+        let data_u8: &[u8; N] = data.try_into().unwrap();
+        let secret_key_u8: &[u8; N] = secret_key.try_into().unwrap();
+        tweetnacl::sign::detached(data_u8, secret_key_u8)
+    }
+
+    fn verify(data: &[u8], signature: &[u8], public_key: &[u8]) -> bool {
+        let data_u8: &[u8; N] = data.try_into().unwrap();
+        let signature_u8: &[u8; N] = signature.try_into().unwrap();
+        let public_key_u8: &[u8; N] = public_key.try_into().unwrap();
+        tweetnacl::sign::detached_verify(data_u8, signature_u8, public_key_u8)
+    }
+
+    ActualMethods {
+        generate,
+        sign,
+        verify,
+    }
+}
 
 #[cfg(not(target_arch = "wasm32"))]
 fn check_fast_signing_rs() -> ActualMethods {
