03f8c66687419fd7d6759c19f9f6770ed5bdb18d
root <rahul.soshte4771@gmail.com>
2023-06-27 13:49:31 +0000

    Added crate description

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
 #![allow(unused)]
-
+//! A low-level library that offers a comprehensive set of functions 
+//! for reading, writing, hashing, and signing primitive XDR constructs 
+//! utilized in the Stellar network.
+//! It provides a nice abstraction for building and signing transactions
 pub mod account;
 pub mod hashing;
 pub mod keypair;
