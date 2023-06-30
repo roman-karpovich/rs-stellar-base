f7d905cb758bce98f91d263606bf45434b1462bd
root <rahul.soshte4771@gmail.com>
2023-06-30 17:17:29 +0000

    Added comments for network.rs

--- src/network.rs
+++ src/network.rs
@@ -1,2 +1,14 @@
-pub static PUBLIC: &str = "Public Global Stellar Network ; September 2015";
-pub static TESTNET: &str = "Test SDF Network ; September 2015";
+//! Contains passphrases for common networks:
+
+/// - `Networks::PUBLIC`: `Public Global Stellar Network ; September 2015`
+/// - `Networks::TESTNET`: `Test SDF Network ; September 2015`
+#[derive(Debug)]
+pub struct Networks;
+
+impl Networks {
+    /// Passphrase for the Public Global Stellar Network, created in September 2015.
+    pub const PUBLIC: &'static str = "Public Global Stellar Network ; September 2015";
+
+    /// Passphrase for the Test SDF Network, created in September 2015.
+    pub const TESTNET: &'static str = "Test SDF Network ; September 2015";
+}
