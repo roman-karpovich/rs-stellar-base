ccbfe3e52b5d29003e83a6f3288399c35f729d5c
root <rahul.soshte4771@gmail.com>
2023-08-10 14:45:45 +0000

    new function for muxed account

--- src/muxed_account.rs
+++ src/muxed_account.rs
@@ -1,32 +1,32 @@
 use stellar_strkey::ed25519::PublicKey;
 
-use crate::{account::Account, utils::decode_encode_muxed_account::encode_muxed_account};
+use crate::{account::Account, utils::decode_encode_muxed_account::{encode_muxed_account, encode_muxed_account_to_address}};
 
 pub struct MuxedAccount {
     account: Account,
-    muxed_xdr: Vec<u8>,
+    muxed_xdr: stellar_xdr::MuxedAccount,
     m_address: String,
     id: String,
 }
 
 impl MuxedAccount {
-    fn new(base_account: Account, id: &str)  { // Result<Self, Box<dyn std::error::Error>>
+    fn new(base_account: Account, id: &str) ->  Result<Self, Box<dyn std::error::Error>>  {
         let account_id = base_account.account_id();
         
         let key = PublicKey::from_string(account_id);
 
         if key.is_err() {
-            // return Err("accountId is invalid".into());
+            return Err("accountId is invalid".into());
         }
 
-        let muxed_xdr = encode_muxed_account(&account_id, id); // Replace with your actual encoding function
-        // let m_address = encode_muxed_account_to_address(&muxed_xdr); // Replace with your actual encoding function
-
-        // Ok(MuxedAccount {
-        //     account: base_account,
-        //     muxed_xdr,
-        //     m_address,
-        //     id: id.to_string(),
-        // })
+        let muxed_xdr = encode_muxed_account(&account_id, id); 
+        let m_address = encode_muxed_account_to_address(&muxed_xdr); 
+        
+        Ok(Self {
+            account: base_account,
+            muxed_xdr,
+            m_address,
+            id: id.to_string(),
+        })
     }
 }
--- src/utils/decode_encode_muxed_account.rs
+++ src/utils/decode_encode_muxed_account.rs
@@ -2,6 +2,9 @@ use std::str::FromStr;
 use arrayref::array_ref;
 use stellar_strkey::ed25519::{MuxedAccount, PublicKey};
 use stellar_xdr::*;
+use stellar_strkey::Strkey::MuxedAccountEd25519;
+
+use crate::muxed_account;
 
 /// Create MuxedAccount obj from string
 pub fn decode_address_to_muxed_account(address: &str) -> MuxedAccount {
@@ -26,4 +29,41 @@ pub fn encode_muxed_account(address: &str, id: &str) -> stellar_xdr::MuxedAccoun
             ed25519: Uint256(*array_ref!(val, 0, 32))
         }
     )
+}
+
+pub fn encode_muxed_account_to_address(muxed_account: &stellar_xdr::MuxedAccount) -> String {
+
+    if muxed_account.discriminant() == stellar_xdr::CryptoKeyType::MuxedEd25519 {
+        return _encode_muxed_account_fully_to_address(muxed_account);
+    }
+
+    let inner_value = match muxed_account {
+        stellar_xdr::MuxedAccount::Ed25519(inner) => inner,
+        _ => panic!("Expected Ed25519 variant"),
+    };
+
+   let key = PublicKey::from_payload(&inner_value.0).unwrap().to_string();
+
+   key
+}
+
+pub fn _encode_muxed_account_fully_to_address(muxed_account: &stellar_xdr::MuxedAccount) -> String {
+    if muxed_account.discriminant() == stellar_xdr::CryptoKeyType::Ed25519 {
+        return encode_muxed_account_to_address(muxed_account);
+    }
+
+    let inner_value = match muxed_account {
+        stellar_xdr::MuxedAccount::MuxedEd25519(inner) => inner,
+        _ => panic!("Expected Ed25519 variant"),
+    };
+
+    let key = &inner_value.ed25519.0;
+
+    let muxed_account = MuxedAccount {
+        ed25519: inner_value.ed25519.0,
+        id: inner_value.id,
+    };
+
+    muxed_account.to_string()
+    
 }
