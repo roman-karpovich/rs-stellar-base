c6fb4df1fff5c6e44216829f79c627ab04969fbf
root <rahul.soshte4771@gmail.com>
2024-01-10 14:28:43 +0000

    updated networks

--- src/network.rs
+++ src/network.rs
@@ -6,11 +6,13 @@
 pub trait NetworkPassphrase {
     fn public() -> &'static str;
     fn testnet() -> &'static str;
+    fn futurenet() -> &'static str;
+    fn sandbox() -> &'static str;
+    fn standalone() -> &'static str;
 }
 
 #[derive(Debug)]
 pub struct Networks;
-
 impl NetworkPassphrase for Networks {
     /// Passphrase for the Public Global Stellar Network, created in September 2015.
     fn public() -> &'static str {
@@ -20,5 +22,17 @@ impl NetworkPassphrase for Networks {
     /// Passphrase for the Test SDF Network, created in September 2015.
     fn testnet() -> &'static str {
         "Test SDF Network ; September 2015"
+    }
+
+    fn futurenet() -> &'static str {
+        "Test SDF Future Network ; October 2022"
+    }
+
+    fn sandbox() -> &'static str {
+        "Local Sandbox Stellar Network ; September 2022"
+    }
+
+    fn standalone() -> &'static str {
+        "Standalone Network ; February 2017"
     }
 }
