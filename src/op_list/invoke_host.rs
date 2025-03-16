bf016dceb582f5d2dab23a65cfa7bec9f6662d20
Vincent Amouret <vincent.amouret@gmail.com>
2025-03-16 11:12:26 +0100

    add tests for invoke_host

--- src/op_list/invoke_host.rs
+++ src/op_list/invoke_host.rs
@@ -115,7 +115,7 @@ impl Operation {
     /// Soroban.
     pub fn wrap_asset(
         &self,
-        asset: Asset,
+        asset: &Asset,
         auth: Option<Vec<xdr::SorobanAuthorizationEntry>>,
     ) -> Result<xdr::Operation, operation::Error> {
         let func = xdr::HostFunction::CreateContract(xdr::CreateContractArgs {
@@ -153,6 +153,7 @@ impl Operation {
 
 #[cfg(test)]
 mod tests {
+    use sha2::digest::crypto_common::Key;
     use stellar_strkey::Strkey;
 
     use crate::contract::ContractBehavior;
@@ -201,5 +202,185 @@ mod tests {
                 }
             }
         }
+    }
+
+    #[test]
+    fn test_invoke_contract() {
+        let contract_id = "CA3D5KRYM6CB7OWQ6TWYRR3Z4T7GNZLKERYNZGGA5SOAOPIFY6YQGAXE";
+
+        let contract = Contracts::new(contract_id).unwrap();
+
+        let op = Operation::new()
+            .invoke_contract(contract_id, "call_me", [].into(), None)
+            .unwrap();
+
+        let cop = contract.call("call_me", None);
+        assert_eq!(op, cop);
+
+        if let xdr::OperationBody::InvokeHostFunction(xdr::InvokeHostFunctionOp {
+            host_function:
+                xdr::HostFunction::InvokeContract(xdr::InvokeContractArgs {
+                    contract_address,
+                    function_name,
+                    args,
+                }),
+            auth,
+        }) = op.body
+        {
+            let exp_contract_address = xdr::ScAddress::from_str(contract_id).unwrap();
+            assert_eq!(contract_address, exp_contract_address);
+
+            let exp_fname = xdr::ScSymbol("call_me".try_into().unwrap());
+            assert_eq!(function_name, exp_fname);
+
+            return;
+        }
+        panic!("Fail")
+    }
+
+    #[test]
+    fn test_invoke_contract_bad_contract_id() {
+        let contract_id = "GA3D5KRYM6CB7OWQ6TWYRR3Z4T7GNZLKERYNZGGA5SOAOPIFY6YQGAXE";
+
+        let op = Operation::new().invoke_contract(contract_id, "call_me", [].into(), None);
+
+        assert_eq!(
+            op.err(),
+            Some(operation::Error::InvalidField("contract_id".into()))
+        );
+    }
+    #[test]
+    fn test_invoke_contract_bad_method() {
+        let contract_id = "CA3D5KRYM6CB7OWQ6TWYRR3Z4T7GNZLKERYNZGGA5SOAOPIFY6YQGAXE";
+
+        let op = Operation::new().invoke_contract(
+            contract_id,
+            "call_me_but_this_is_a_too_long_method",
+            [].into(),
+            None,
+        );
+
+        assert_eq!(
+            op.err(),
+            Some(operation::Error::InvalidField("method".into()))
+        );
+    }
+
+    #[test]
+    fn test_create_contract() {
+        let deployer = Keypair::random().unwrap().public_key();
+        let wasm_hash = [0; 32];
+        let salt = Keypair::random().unwrap().raw_pubkey();
+        let op = Operation::new()
+            .create_contract(&deployer, wasm_hash, Some(salt), None, [].into())
+            .unwrap();
+
+        if let xdr::OperationBody::InvokeHostFunction(xdr::InvokeHostFunctionOp {
+            host_function:
+                xdr::HostFunction::CreateContractV2(xdr::CreateContractArgsV2 {
+                    contract_id_preimage:
+                        xdr::ContractIdPreimage::Address(xdr::ContractIdPreimageFromAddress {
+                            address,
+                            salt: actual_salt,
+                        }),
+                    executable,
+                    constructor_args,
+                }),
+            auth,
+        }) = op.body
+        {
+            assert_eq!(address, xdr::ScAddress::from_str(&deployer).unwrap());
+            assert_eq!(actual_salt, xdr::Uint256(salt));
+            assert_eq!(
+                executable,
+                xdr::ContractExecutable::Wasm(xdr::Hash(wasm_hash))
+            );
+            //
+            return;
+        }
+        panic!("Fail")
+    }
+    #[test]
+    fn test_create_contract_default_salt() {
+        let deployer = Keypair::random().unwrap().public_key();
+        let wasm_hash = [0; 32];
+        let op = Operation::new()
+            .create_contract(&deployer, wasm_hash, None, None, [].into())
+            .unwrap();
+
+        if let xdr::OperationBody::InvokeHostFunction(xdr::InvokeHostFunctionOp {
+            host_function:
+                xdr::HostFunction::CreateContractV2(xdr::CreateContractArgsV2 {
+                    contract_id_preimage:
+                        xdr::ContractIdPreimage::Address(xdr::ContractIdPreimageFromAddress {
+                            address,
+                            salt: actual_salt,
+                        }),
+                    executable,
+                    constructor_args,
+                }),
+            auth,
+        }) = op.body
+        {
+            assert_eq!(address, xdr::ScAddress::from_str(&deployer).unwrap());
+            assert_ne!(actual_salt, xdr::Uint256([0; 32]));
+            assert_eq!(
+                executable,
+                xdr::ContractExecutable::Wasm(xdr::Hash(wasm_hash))
+            );
+            //
+            return;
+        }
+        panic!("Fail")
+    }
+
+    #[test]
+    fn test_create_contract_bad_deployer() {
+        let deployer = Keypair::random().unwrap().public_key().replace("G", "M");
+        let wasm_hash = Keypair::random().unwrap().raw_pubkey();
+        let op = Operation::new().create_contract(&deployer, wasm_hash, None, None, [].into());
+
+        assert_eq!(
+            op.err(),
+            Some(operation::Error::InvalidField("deployer".into()))
+        );
+    }
+
+    #[test]
+    fn test_wrap_asset() {
+        let native = Asset::native();
+
+        let op = Operation::new().wrap_asset(&native, None).unwrap();
+        if let xdr::OperationBody::InvokeHostFunction(xdr::InvokeHostFunctionOp {
+            host_function:
+                xdr::HostFunction::CreateContract(xdr::CreateContractArgs {
+                    contract_id_preimage: xdr::ContractIdPreimage::Asset(asset),
+                    executable: xdr::ContractExecutable::StellarAsset,
+                }),
+            auth,
+        }) = op.body
+        {
+            assert_eq!(native.to_xdr_object(), asset);
+            //
+            return;
+        }
+        panic!("Fail")
+    }
+
+    #[test]
+    fn test_upload_wasm() {
+        let wasm = [0; 420];
+        let op = Operation::new().upload_wasm(&wasm, None).unwrap();
+
+        if let xdr::OperationBody::InvokeHostFunction(xdr::InvokeHostFunctionOp {
+            host_function: xdr::HostFunction::UploadContractWasm(bytes),
+            auth,
+        }) = op.body
+        {
+            assert_eq!(bytes.as_slice(), &wasm);
+            //
+            return;
+        }
+        panic!("Fail")
     }
 }
