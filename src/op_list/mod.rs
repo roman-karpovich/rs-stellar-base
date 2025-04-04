97fb4faf4fbaaaa9a6e9299464a967a046be07a1
Vincent Amouret <vincent.amouret@gmail.com>
2025-04-04 17:26:18 +0200

    extend and restore footprint

--- src/op_list/mod.rs
+++ src/op_list/mod.rs
@@ -3,10 +3,11 @@ pub mod account_merge;
 pub mod bump_sequence;
 pub mod create_account;
 pub mod create_passive_sell_offer;
+pub mod extend_footprint_ttl;
 pub mod invoke_host;
-pub mod path_payment_strict_receive;
-pub mod path_payment_strict_send;
 pub mod manage_buy_offer;
 pub mod manage_sell_offer;
+pub mod path_payment_strict_receive;
+pub mod path_payment_strict_send;
 pub mod payment;
-
+pub mod restore_footprint;
