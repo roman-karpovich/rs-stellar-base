9498bd4486420383521fd24259f7ac751e62ea20
Vincent Amouret <vincent.amouret@gmail.com>
2025-04-07 19:17:18 +0200

    tests for withdraw

--- src/op_list/liquidity_pool_withdraw.rs
+++ src/op_list/liquidity_pool_withdraw.rs
@@ -16,6 +16,17 @@ impl Operation {
         hex::decode_to_slice(pool_id, &mut h)
             .map_err(|_| operation::Error::InvalidField("pool_id".into()))?;
         let liquidity_pool_id = xdr::PoolId(xdr::Hash(h));
+
+        if amount < 0 {
+            return Err(operation::Error::InvalidAmount(amount));
+        }
+        if min_amount_a < 0 {
+            return Err(operation::Error::InvalidAmount(min_amount_a));
+        }
+        if min_amount_b < 0 {
+            return Err(operation::Error::InvalidAmount(min_amount_b));
+        }
+
         let body = xdr::OperationBody::LiquidityPoolWithdraw(xdr::LiquidityPoolWithdrawOp {
             liquidity_pool_id,
             amount,
@@ -26,5 +37,112 @@ impl Operation {
             source_account: self.source.clone(),
             body,
         })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{
+        operation::{self, Operation},
+        xdr,
+    };
+
+    #[test]
+    fn test_lp_withdraw() {
+        let pool_id = hex::encode([8; 32]);
+        let amount = 50;
+        let min_amount_a = 12 * operation::ONE;
+        let min_amount_b = 40 * operation::ONE;
+
+        let op = Operation::new()
+            .liquidity_pool_withdraw(&pool_id, amount, min_amount_a, min_amount_b)
+            .unwrap();
+
+        if let xdr::OperationBody::LiquidityPoolWithdraw(xdr::LiquidityPoolWithdrawOp {
+            liquidity_pool_id: xdr::PoolId(xdr::Hash(h)),
+            amount,
+            min_amount_a: min_a,
+            min_amount_b: min_b,
+        }) = op.body
+        {
+            assert_eq!(h, [8; 32]);
+            assert_eq!(min_a, min_amount_a);
+            assert_eq!(min_b, min_amount_b);
+            //
+        } else {
+            panic!("Fail")
+        }
+    }
+    #[test]
+    fn test_lp_withdraw_bad_id() {
+        let pool_id = hex::encode([8; 33]);
+        let amount = 50;
+        let min_amount_a = 12 * operation::ONE;
+        let min_amount_b = 40 * operation::ONE;
+
+        let op =
+            Operation::new().liquidity_pool_withdraw(&pool_id, amount, min_amount_a, min_amount_b);
+
+        assert_eq!(
+            op.err(),
+            Some(operation::Error::InvalidField("pool_id".into()))
+        );
+    }
+    #[test]
+    fn test_lp_withdraw_bad_id2() {
+        let pool_id = hex::encode([8; 31]);
+        let amount = 50;
+        let min_amount_a = 12 * operation::ONE;
+        let min_amount_b = 40 * operation::ONE;
+
+        let op =
+            Operation::new().liquidity_pool_withdraw(&pool_id, amount, min_amount_a, min_amount_b);
+
+        assert_eq!(
+            op.err(),
+            Some(operation::Error::InvalidField("pool_id".into()))
+        );
+    }
+    #[test]
+    fn test_lp_withdraw_bad_amount() {
+        let pool_id = hex::encode([8; 32]);
+        let amount = -50;
+        let min_amount_a = 12 * operation::ONE;
+        let min_amount_b = 40 * operation::ONE;
+
+        let op =
+            Operation::new().liquidity_pool_withdraw(&pool_id, amount, min_amount_a, min_amount_b);
+
+        assert_eq!(op.err(), Some(operation::Error::InvalidAmount(amount)));
+    }
+    #[test]
+    fn test_lp_withdraw_bad_amount2() {
+        let pool_id = hex::encode([8; 32]);
+        let amount = 50;
+        let min_amount_a = -12 * operation::ONE;
+        let min_amount_b = 40 * operation::ONE;
+
+        let op =
+            Operation::new().liquidity_pool_withdraw(&pool_id, amount, min_amount_a, min_amount_b);
+
+        assert_eq!(
+            op.err(),
+            Some(operation::Error::InvalidAmount(min_amount_a))
+        );
+    }
+    #[test]
+    fn test_lp_withdraw_bad_amount3() {
+        let pool_id = hex::encode([8; 32]);
+        let amount = 50;
+        let min_amount_a = 12 * operation::ONE;
+        let min_amount_b = -40 * operation::ONE;
+
+        let op =
+            Operation::new().liquidity_pool_withdraw(&pool_id, amount, min_amount_a, min_amount_b);
+
+        assert_eq!(
+            op.err(),
+            Some(operation::Error::InvalidAmount(min_amount_b))
+        );
     }
 }
